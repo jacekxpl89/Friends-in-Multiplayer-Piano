e65cfbabbdcb2798b249052d7b057c89bab771dd
MajorH5 <73318032+MajorH5@users.noreply.github.com>
2021-01-03 14:23:02 -0500

    Update Friends_in_MPP.js
    - 1/3/2021
    New update with messaging

--- Friends_in_MPP.js
+++ Friends_in_MPP.js
@@ -19,12 +19,21 @@ let bgr = []
 let savname = [];
 let friendsWindow = 'friendsWindow';
 let friendsButton = 'friendsButton';
+let verifyUserPrompt = 'verifyUserPrompt';
+let cancelButton = 'cancelButton';
 let cookieFriendLocation;
 let addFriend = 'addFriend';
+let connectingText
+let userVerifyCode
+let messagingInfo;
 let cookiesFound = false;
+let WEBSOCKETLOCATION = 'wss://mppchatclient.info:8080/';
+let clickable=true
 let iloc;
 let jloc;
 let gBS;
+let messengerSetup = [false];
+let ownid
 
 let deleteCookie = 'Thu, 01 Jan 1970 00:00:01 GMT,';
 let nameDiv = [];
@@ -136,6 +145,7 @@ function checkFriendHTML(playerid, p) {
 // -- //
 // ADD FRIEND MENU ITEM:
 MPP.client.on('participant added', (p) => {
+	ownid = (MPP.client.getOwnParticipant()._id)
 	checkFriendHTML(p._id, p)
 	setTimeout(() => {
 		p.nameDiv.addEventListener('mousedown', () => {
@@ -264,7 +274,7 @@ function showObject(visibility, object) {
 
 // -- //
 // UNIVERSAL CREATE DIALOG WINDOW:
-function cDW(object, l, t, h, visibility) {
+function cDW(object, l, t, h, inner, scroll, visibility) {
 	if (typeof visibility !== 'number') {
 		visibility = 0
 	}
@@ -272,20 +282,20 @@ function cDW(object, l, t, h, visibility) {
 	object = document.createElement('div')
 	object.id = (name + "-window");
 	object.className = "dialog";
-	object.style = 'overflow: hidden scroll; visibility: visible;';
+	object.style = `${scroll}`;
 	object.style.position = 'absolute';
 	object.style.display = 'block';
 	object.style.height = (h + 'px');
 	object.style.marginLeft = (l + 'px');
 	object.style.marginTop = (t + 'px');
-	object.innerHTML = 'Friends';
+	object.innerHTML = inner;
 	document.getElementsByClassName('relative')[visibility].appendChild(object);
 };
 // -- //
 
 // -- //
 // UNIVERSAL CREATE BUTTONS:
-function cB(object, l, t, i, visibility, num) {
+function cB(object, l, t, i, visibility, num, loc) {
 	if (typeof visibility !== 'number') {
 		visibility = 0
 	};
@@ -303,6 +313,114 @@ function cB(object, l, t, i, visibility, num) {
 	document.getElementsByClassName('relative')[visibility].appendChild(object);
 };
 // -- //
+console.log(ownid)
+
+
+// DO NOT CHANGE, THIS PREVENTS USER FROM SPAMMING SERVER WITH WEBSOCKET CONNECTION REQUESTS.
+function clickBuffer(){
+	clickable=false
+	setTimeout(function(){ 
+	clickable=true
+	}, 30000);
+}
+
+//
+let stopDots
+function informUser(){
+	if(authenticationStatus===undefined || authenticationStatus===null){
+		stopDots=true
+		connectingText.style.color = 'red';
+		connectingText.innerHTML = "AUTHENTICATION FAILED. REASON: TIMEOUT";
+		cancelButton.style.color = 'red';
+		cancelButton.innerHTML = 'Failure!';
+		}
+	if(authenticationStatus===true){
+		stopDots=true
+		connectingText.style.color = 'lime';
+		connectingText.innerHTML = "AUTHENTICATION SUCCESS. IP TIED TO ID.";
+		cancelButton.style.color = 'lime';
+		cancelButton.innerHTML = 'Success!';
+	}
+	if(authenticationStatus===false){
+		stopDots=true
+		connectingText.style.color = 'red';
+		connectingText.innerHTML = "AUTHENTICATION FAILED. REASON: AUTHORIZATION CODE ERROR.";
+		cancelButton.style.color = 'red';
+		cancelButton.innerHTML = 'Failure!';
+	}
+}
+
+
+// ALLOWS SENDING MESSAGE THROUGH WEBSOCKET:
+//
+let verifyRoom
+let verifyCode
+let authenticationStatus
+function sendMessage() {
+	console.log('opening websocket')
+	if (clickable===false){
+		console.log('DO NOT SPAM WEBSOCKET WITH REQUESTS.')
+		return
+	}
+	clickBuffer()
+	const ws = new WebSocket(`${WEBSOCKETLOCATION}`);
+	if (messengerSetup[0] === false) {
+		ws.onopen = function () {
+			console.log('WebSocket Client Connected');
+			ws.send(`mSETUP-${ownid}`);
+		};
+
+		ws.onmessage = function (e) {
+			if (e.data.startsWith('*')){
+				verifyCode=e.data
+				console.log(verifyCode)
+				console.log('received the verify code')
+			}
+			if (e.data.startsWith('!')){
+				console.log('received the verify room')
+				verifyRoom=e.data.substr(1, 26)
+				console.log(verifyRoom)
+				setTimeout(function(){
+					ws.close()
+					var client = new Client('wss://www.multiplayerpiano.com:443');
+					client.start();
+					client.setChannel(verifyRoom);
+					client.on('ch', () => {
+						console.log('SENDING CODE')
+						client.sendArray([{m:'a', message: `${verifyCode}`}]);
+						client.on('a',msg=>{
+							if(msg.a.toString().toLowerCase().startsWith('user authenticated.')) {
+							console.log('AUTHENTICATION SUCCESS')
+							authenticationStatus=true
+							informUser()
+							ws.close()
+							client.stop();	
+							}
+							if(msg.a.toString().toLowerCase().startsWith('could not authenticate.')) {
+							console.log('AUTHENTICATION FAILURE')
+							authenticationStatus=false
+							informUser()
+							ws.close()
+							client.stop();	
+							}
+						});
+						setTimeout(function(){
+							if(authenticationStatus==undefined || authenticationStatus==null){
+								console.log('TIMEOUT')
+								informUser()
+								ws.close()
+								client.stop();
+							}
+						}, 30000);
+					});
+				}, 1000);
+			}
+		}
+	};
+
+}
+//
+
 
 // -- //
 // ADDS CLICK TO FRIEND PANEL
@@ -317,8 +435,51 @@ function addClick(object, playerid, p) {
 			button1.innerHTML = 'Message';
 			document.getElementById(playerid).appendChild(button1);
 			button1.addEventListener('click', () => {
-				console.log('Feature has not been added yet!')
-			}, 24)
+				if (document.getElementById('verifyUserPrompt-window')===null){
+				sendMessage()
+
+				cDW(verifyUserPrompt, '-215', '-693', '400', 'Verify your player ID', 'visibility: visible;overflow-wrap: anywhere' )
+
+				cancelButton = document.createElement('div');
+				cancelButton.id = ("cancelButton-btn");
+				cancelButton.className = 'ugly-button';
+				cancelButton.style.position = 'absolute';
+				cancelButton.style.left = ('147px');
+				cancelButton.style.top = ('372px');
+				cancelButton.innerHTML = 'Cancel';
+
+
+				messagingInfo = document.createElement('div');
+				messagingInfo.id = ('messagingInfo');
+				messagingInfo.style.position = 'absolute';
+				messagingInfo.style.left = ('8px');
+				messagingInfo.style.fontSize = ('large');
+				messagingInfo.style.top = ('61px');
+				messagingInfo.innerHTML = 'In order to enable direct messaging in MPP you must connect to an external socket. For security reasons you must verify your id. This is a one time process and will permanently assign your IP address to your ID to ensure that messages sent by your ID is actually you. Please wait for the script to connect to websocket.';
+				
+				connectingText = document.createElement('div');
+				connectingText.id = ('connectingText');
+				connectingText.style.position = 'absolute';
+				connectingText.style.left = ('28px');
+				connectingText.style.fontSize = ('large');
+				connectingText.style.top = ('247px');
+				connectingText.innerHTML = `Please wait, connecting to websocket`
+				document.getElementById('verifyUserPrompt-window').appendChild(cancelButton);
+				document.getElementById('verifyUserPrompt-window').appendChild(messagingInfo);
+				document.getElementById('verifyUserPrompt-window').appendChild(connectingText);
+				var dots = window.setInterval( function() {
+					if ( connectingText.innerHTML.length > 39 ){
+						if (stopDots===true){return}
+					connectingText.innerHTML = "Please wait, connecting to websocket";
+					}else{ 
+						if (stopDots===true){return}
+					connectingText.innerHTML += ".";
+					}}, 100);
+
+				cancelButton.onclick = () => {
+					document.getElementById('verifyUserPrompt-window').remove()
+				};
+			}}, 24)
 			let button2 = document.createElement("div")
 			button2.className = 'ugly-button';
 			button2.id = `${playerid}_btn2`
@@ -459,7 +620,7 @@ function removeFromPanel(playerid) {
 	setTimeout(() => {
 		loadToPanel()
 	}, 1000);
-	cDW(friendsWindow, '500', '-400', '400');
+	cDW(friendsWindow, '500', '-400', '400', 'Friends', 'overflow: hidden scroll; visibility: visible');
 	cB(friendsButton, '660', '32', 'Friends', 0, 1);
 })();
 // -- //
